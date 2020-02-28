f2ab754ce0d7a0f9a4e6240a753aea5ce5c01f53
citycomp <citycomp@DESKTOP-6ITFH69>
2020-02-28 12:40:58 +0200

    test3 Salsal

---
+++ src/test3/add.java
@@ -0,0 +1,11 @@
+package test3;
+
+public class add {
+	public static int addFun(int a, int b) {
+		return a+b;
+	}
+	
+public static void main(String argu[]) {
+	System.out.println(addFun(4,5));
+}
+}
